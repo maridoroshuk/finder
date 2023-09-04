54d83cab497d809d13d2d00e0ed542c5d834d4b8
maryna <59647513+maridoroshuk@users.noreply.github.com>
2023-09-04 16:42:29 +0200

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <main>
       <HomePage />
-    </>
+    </main>
   );
 }
 
--- src/pages/home/tiles/Tiles.tsx
+++ src/pages/home/tiles/Tiles.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Tiles = () => {
-  return <div>Hello</div>;
+  return <></>;
 };
