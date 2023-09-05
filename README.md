be2faa0777f421ad54d5e36255bd424f6a29c87b
maryna <59647513+maridoroshuk@users.noreply.github.com>
2023-09-05 10:07:59 +0200

    clean

--- README.md
+++ README.md
@@ -2,22 +2,15 @@
  
 ## Installation
 
-Use the package manager [yarn](https://classic.yarnpkg.com/lang/en/docs/install/#windows-stable)/[npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm) to install.
+Use the package manager [yarn](https://classic.yarnpkg.com/lang/en/docs/install/#windows-stable) to install.
 
 ```bash
 yarn install
 ```
 
-```bash
-npm install
-```
 
 ## Usage
 
 ```bash
 yarn run dev
-```
-
-```bash
-npm run dev
 ```
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Tiles } from 'pages/home/tiles/Tiles';
+import { HomeTitle } from 'pages/home/title/HomeTitle';
 
 import cn from 'pages/home/Homepage.module.scss';
 
 export const HomePage = () => {
   return (
     <div className={cn.content}>
-      <h2 className={cn.title}>Join 2 million+ Australians finding better</h2>
-      <p className={cn.description}>
-        Finder's team of 40+ experts will help you find the right choices in
-        over 100 categories.
-      </p>
+      <HomeTitle />
       <Tiles />
     </div>
   );
